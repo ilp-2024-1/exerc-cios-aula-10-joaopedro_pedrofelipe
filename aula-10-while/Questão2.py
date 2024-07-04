92f97f4ccc12d16c5cdc09a0580f5045d9b3157b
joaoptviera <teixeira.vieira@escolar.ifrn.edu.br>
2024-07-04 09:40:04 -0300

    modified:   "aula-10-while/Quest\303\243o2.py" 	modified:   "aula-10-while/Quest\303\243o3.py"

---
+++ aula-10-while/Questão2.py
@@ -0,0 +1,5 @@
+#Escreva um programa que solicita ao usuário valores numéricos e realiza a soma
+#desses valores. Quando o usuário digitar o valor 0 o programa deverá exibir o valor do
+#somatório e encerrar o programa com uma mensagem de término do programa. O
+#usuário deverá ser informado no início do programa o que o programa faz e qual a
+#condição para encerramento do programa.
---
+++ aula-10-while/Questão3.py
@@ -0,0 +1,5 @@
+#Crie um programa que solicita ao usuário uma senha e em seguida compare esse valor
+#com uma senha armazenada em uma variável. Enquanto o usuário não acertar o valor
+#da senha o programa deverá solicitar a senha ao usuário. Quando o usuário acerta a
+#senha, o programa deverá encerrar exibindo uma mensagem encerramento e informar
+#que o usuário acertou a senha.
