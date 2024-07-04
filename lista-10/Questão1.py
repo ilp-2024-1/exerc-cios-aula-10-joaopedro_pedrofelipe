191c45f9724fbb57091983ccea0bed115b870d9a
joaoptviera <teixeira.vieira@escolar.ifrn.edu.br>
2024-07-04 09:32:57 -0300

    modified:   "lista-10/Quest\303\243o1.py"

--- lista-10/Questão1.py
+++ lista-10/Questão1.py
@@ -1,1 +1,14 @@
-print('sla')
+#Escreva um programa que exiba os valores dos números de 1 até 100. Ao término o
+#programa deverá exibir uma mensagem de encerramento informando que o programa
+#terminou.
+
+num = 0
+
+botao = input('Digite s para iniciar: ')
+
+if(botao == 's'):
+    while (num < 100):
+        num = num + 1
+        print(num)
+print('fim do prigrama')
+
