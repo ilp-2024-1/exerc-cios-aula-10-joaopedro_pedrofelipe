b77ba6c12759b3f5c19b8317ec4f825fd215f2fd
joaoptviera <teixeira.vieira@escolar.ifrn.edu.br>
2024-07-10 11:56:27 -0300

    modified:   "aula-10-while/Quest\303\243o4.py"

--- aula-10-while/Questão4.py
+++ aula-10-while/Questão4.py
@@ -2,3 +2,17 @@
 #para adivinhar qual é o número. O programa deve continuar pedindo palpites até que
 #o usuário acerte o número. Ao final, o programa deve informar quantos palpites foram
 #necessários e informar que o programa encerrou.
+
+import random
+
+num_ale = random.randint(0, 10)
+print(num_ale)
+
+num_test = int(input('Advinhe o numero: '))
+
+while(num_ale != num_test):
+    num_test = int(input('tente novamente: '))
+
+print('Você acertou')
+
+print('Fim do programa')
